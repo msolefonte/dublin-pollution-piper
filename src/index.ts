665ef00f7e9cd9d8bca0a203295c4302986604bf
msolefonte <19@370W^PpNs>
2021-11-04 19:12:00 +0000

    sync

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import {OpenSensorWebClient} from "./lib/OpenSensorWebClient";
+// import {OpenSensorWebClient} from "./lib/OpenSensorWebClient";
+import {TomTomTrafficClient} from "./lib/TomTomTrafficClient";
 
-const oswc = new OpenSensorWebClient('STA.IE.IE0098A', 'SPO.IE.IE0098ASample1_8-NO2-PT1H');
-oswc.fetch_data(168).then((data) => {
+// const oswc = new OpenSensorWebClient('STA.IE.IE0098A', 'SPO.IE.IE0098ASample1_8-NO2-PT1H');
+const oswc = new TomTomTrafficClient('FQXYF9UX1peapMlODDER3MQSGJbv1Yx1', '53.346019,-6.293357', 22);
+oswc.fetchData().then((data) => {
     console.log(data);
 });
